934a602105aef5c00f9f315f5cbfea71546337c6
Dhananjoy Bhuyan <dhananjoy.bhuyan.2022@gmail.com>
2025-03-05 22:23:41 +0530

    Update haha!!

--- pyautotranslate.py
+++ pyautotranslate.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jan 31 21:01:41 2025
@@ -37,7 +38,7 @@ def _hfAsk(prompt: str = "hello",
 
     client = InferenceClient(
         model="Qwen/Qwen2.5-Coder-32B-Instruct",
-        token='hugging_face token api',
+        token='hugging face API token',
     )
     response_text = []
     for message in client.chat_completion(
@@ -112,69 +113,4 @@ def execute_code():
                             extension=EXTENSION, lang=LANG)
     else:
         ValueError("Maybe our interpreter could not understand your code, please write it in a more understandable way or please write it in plain english.")
-        messages=MESSAGES,
-        **params
-    ):
-        token_content = message.choices[0].delta.content
-        response_text.append(token_content)
 
-    response_text = "".join(response_text)
-    return response_text
-
-
-def _validate_filename(file_name: str):
-    symbols = list(string.punctuation)
-    symbols.remove('_')
-    for i in symbols:
-        if i in file_name and i != ' ':
-            raise ValueError(
-                "There should be no file extension or any punctuation or symbols in the file name just give the file_name, THAT'S IT!!!!!!")
-    return file_name
-
-
-def _translate_to_lang(file_path: str, lang: str):
-    with open(file_path, "r") as f:
-        code = f.read()
-    code_start = code.find('"""pycode') + 9
-    code_end = code.find('"""', code_start)
-    code = code[code_start:code_end]
-    converted_code = _hfAsk(
-        code, system_prompt=f"You are an AI interpreter, so people give you code and you just translate it into {lang}, the code is kinda different or even can be plain english or kinda psuedo code, Any kind. But you understand what the code means, then you translate to {lang} only.")
-
-    if converted_code and "```" in converted_code:
-        code_start = converted_code.find("```") + 7
-        code_end = converted_code.find("```", code_start)
-        code = converted_code[code_start:code_end]
-    else:
-        return None
-    return code
-
-
-def _save_and_open_code(code: str, file_name: str, extension: str):
-    path = os.path.abspath(f"./{file_name}.{extension}")
-    with open(f"./{file_name}.{extension}", "w") as f:
-        f.write(code)
-
-
-LANG = None
-EXTENSION = None
-
-
-def init(file_name: str, extension_of_output_file: str, language: str):
-    global LANG
-    global FILE_NAME
-    global EXTENSION
-    FILE_NAME = _validate_filename(file_name)
-    LANG = language
-    if '.' in extension_of_output_file:
-        extension_of_output_file = extension_of_output_file.replace('.', '')
-    EXTENSION = extension_of_output_file
-
-
-def execute_code():
-    file_path = inspect.stack()[1].filename
-    code = _translate_to_lang(file_path, LANG)
-    if code:
-        _save_and_open_code(code, file_name=FILE_NAME, extension=EXTENSION)
-    else:
-        ValueError("Maybe our interpreter could not understand your code, please write it in a more understandable way or please write it in plain english.")
